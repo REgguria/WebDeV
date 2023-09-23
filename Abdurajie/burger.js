c96c27afbb6126ebf06534b0dd65b0d0427a66e7
REgguria <145839362+REgguria@users.noreply.github.com>
2023-09-23 17:57:50 +0800

    Delete Abdurajie directory

--- Abdurajie/burger.js
+++
@@ -1,9 +0,0 @@
-function showmenu()
-{
-    var x = document.getElementById('myUL');
-    if (x.style.display == 'none') {
-        x.style.display = 'flex';
-    } else {
-        x.style.display = 'none';
-    }
-}
